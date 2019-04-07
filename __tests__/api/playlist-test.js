b4a757140dfa35b2c0ef212c14f0c8f542ad3602
Kevin Prabhakar <kprabhakar@Kevins-MacBook-Pro.local>
2019-04-06 17:06:20 -0700

    testing

--- __tests__/api/playlist-test.js
+++ __tests__/api/playlist-test.js
@@ -4,7 +4,7 @@ const { Joi } = frisby;
 it('should return a 404 when deleting a playlist that does not exist',()=>{
     return frisby
     .del('http://localhost:8000/api/playlists/-1')
-    .expect('status',404); 
+    .expect('status',404);  
 });
 
 // it('should return a 204 when deleting a playlist that does exist',()=>{
